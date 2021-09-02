2114d77640845719b3cb54ad2d3c1e2141a07d4c
Tevinthuku <tevinthuku@gmail.com>
2021-09-02 08:02:00 +0300

    tests refactor

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,6 +1,4 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
+use actix_web::{HttpRequest, HttpResponse, Responder};
 
 pub async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
